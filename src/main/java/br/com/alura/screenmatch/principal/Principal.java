cb1eade8e1045291b9d750a283425719175ae9c8
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-07-11 14:51:58 -0400

    first commit

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -20,36 +20,47 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
 
     public void exibeMenu() {
-        var menu = """
+        var opcao = -1;
+
+        while(opcao != 0) {
+            var menu = """
                 1 - Buscar séries
                 2 - Buscar episódios
+                3 - Listar séries buscadas
                 
                 0 - Sair                                 
                 """;
 
-        System.out.println(menu);
-        var opcao = leitura.nextInt();
-        leitura.nextLine();
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
 
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
+
     }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        this.dadosSeries.add(dados);
         System.out.println(dados);
     }
 
@@ -71,5 +82,9 @@ public class Principal {
             temporadas.add(dadosTemporada);
         }
         temporadas.forEach(System.out::println);
+    }
+
+    private void listarSeriesBuscadas() {
+        this.dadosSeries.forEach(System.out::println);
     }
 }
